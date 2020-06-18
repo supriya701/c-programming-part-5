d230e181ee99985f6a70b776bff75745fef57b16
supriya701 <61152281+supriya701@users.noreply.github.com>
2020-06-18 21:01:56 +0530

    Add files via upload

---
+++ reverse_string.cpp
@@ -0,0 +1,25 @@
+//program to reverse a string in c
+
+#include <stdio.h>
+int main()
+{
+    char str[1000], rev[1000];
+    int i, j, count = 0;
+    scanf("%s", str);
+
+    //#finding the length of the string
+    while (str[count] != NULL)
+    {
+        count++;
+    }
+    j = count - 1;
+
+    //#reversing the string by swapping
+    for (i = 0; i < count; i++) 
+    {
+        rev[i] = str[j];
+        j--;
+    }
+
+    printf(rev);
+}
